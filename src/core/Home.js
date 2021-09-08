ff6f7775acc66bfac7fe3bc47086bda6e321b28d
abs3nce <vodrazka.jakub@gmail.com>
2021-09-08 21:50:30 +0200

    + router, bootstrap

---
+++ src/core/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div className="Jumbotron bg-light p-5">
+    <h2>Home</h2>
+    <p className="lead">Welcome to this Frontend</p>
+  </div>
+);
+
+export default Home;
