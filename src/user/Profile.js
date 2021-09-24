2c98c319489d4c56b25eb642bc178eb71283b987
abs3nce <vodrazka.jakub@gmail.com>
2021-09-24 15:55:44 +0200

    - bootstrap changes, profile management button

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router";
+import { Redirect, Link } from "react-router-dom";
 import { isAuthenticated } from "../auth/";
 import { read } from "./apiController";
 
@@ -35,21 +35,43 @@ class Profile extends Component {
   }
 
   render() {
-    const redirectToLogin = this.state.redirectToLogin;
+    const { redirectToLogin, user } = this.state;
     if (redirectToLogin) return <Redirect to="/login" />;
 
     return (
       <div className="container">
-        <h2 className="mt-5 mb-5">Profile Page</h2>
-        <p>Hello {isAuthenticated().user.username}</p>
-        <p>Email is {isAuthenticated().user.email}</p>
-        <p>
-          {" "}
-          {`Registered on ${new Date(this.state.user.created).toDateString()}`}
-        </p>
-        <p>{`Has been a member for ${this.registeredFor(
-          this.state.user.created
-        )} days`}</p>
+        <div className="row">
+          <h2 className="mt-5 mb-5">Profile Page</h2>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <p>Hello {isAuthenticated().user.username}</p>
+            <p>Email is {isAuthenticated().user.email}</p>
+            <p> {`Registered on ${new Date(user.created).toDateString()}`}</p>
+            <p>{`Has been a member for ${this.registeredFor(
+              user.created
+            )} days`}</p>
+          </div>
+          <div className="col-md-6">
+            {isAuthenticated().user && isAuthenticated().user._id === user._id && (
+              <div className="row text-center">
+                <div className="col-md-6">
+                  <Link
+                    className="btn btn-raised btn-success"
+                    to={`/user/edit/${user._id}`}
+                  >
+                    EDIT PROFILE
+                  </Link>
+                </div>
+                <div className="col-md-6">
+                  <button className="btn btn-raised btn-danger">
+                    DELETE PROFILE
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
