d0eafb574b04856346cdfafe891c70f514b283a0
abs3nce <vodrazka.jakub@gmail.com>
2021-11-26 14:29:27 +0100

    + minor design changes

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { resetPassword } from "../controllers/auth";
+import {Link} from 'react-router-dom'
 
 class ResetPassword extends Component {
     constructor(props) {
@@ -74,7 +75,7 @@ class ResetPassword extends Component {
                         >
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
                         </svg>
-                        <div className="m-2">{message}</div>
+                        <div className="m-2">{message} <Link to="/login">here</Link></div>
                     </div>
                 ) : (
                     ""
